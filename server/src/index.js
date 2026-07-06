f36c440e2461033bf2ad6330d804b36c0f5d4f9f
Asad <asad2747982@gmail.com>
2026-07-06 16:05:52 +0500

    error fix

--- server/src/index.js
+++ server/src/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to the API" });
+  res.send({ message: "Welcome to the API" });
 });
 
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
