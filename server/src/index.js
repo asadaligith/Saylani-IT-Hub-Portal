430fd56bd69d9f9fcff0e5c9543fab1a289f7dbd
Asad <asad2747982@gmail.com>
2026-07-06 16:00:55 +0500

    backend api error fix

--- server/src/index.js
+++ server/src/index.js
@@ -19,6 +19,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the API" });
+});
+
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
 app.use("/api/auth", authRoutes);
